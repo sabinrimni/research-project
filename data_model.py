837149c8b0fdfd18dae1e2dd4dba0e763ce66d67
antaranin <rafal@getlevelup.com>
2019-11-18 20:23:18 +0100

    Made the thing find the operations. Crashes on some inputs.

--- data_model.py
+++ data_model.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Transformation:
     inflection: str
     lemma: str
@@ -14,3 +17,40 @@ class Transformation:
         self.inflection = inflection
         self.lemma = lemma
         self.rules = rules
+
+
+class Operation:
+    index: int
+    letters: str
+
+    def __init__(self, index: int, letters: str) -> None:
+        super().__init__()
+        self.index = index
+        self.letters = letters
+
+    def __str__(self) -> str:
+        return f"(index: {self.index}, letters: {self.letters})"
+
+    def __repr__(self) -> str:
+        return f"(index: {self.index}, letters: {self.letters})"
+
+
+class Operations:
+    transformation: Transformation
+    inserts: List[Operation]
+    deletes: List[Operation]
+
+    def __init__(self, transformation: Transformation, inserts: List[Operation],
+                 deletes: List[Operation]) -> None:
+        super().__init__()
+        self.transformation = transformation
+        self.inserts = inserts
+        self.deletes = deletes
+
+    def __str__(self) -> str:
+        return f"(transformation: {self.transformation}, inserts: {self.inserts}, deletes: {self.deletes})"
+
+    def __repr__(self) -> str:
+        return f"(transformation: {self.transformation}, inserts: {self.inserts}, deletes: {self.deletes})"
+
+
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from typing import List
 
-from data_model import Transformation
+from data_model import Transformation, Operation, Operations
 import Bio.pairwise2 as pair
-from Bio.SubsMat import MatrixInfo as matlist
+import regex as re
+import more_itertools as mit
 
 
 def read_file_data(file_name: str) -> List[Transformation]:
@@ -16,10 +17,45 @@ def read_file_data(file_name: str) -> List[Transformation]:
     return data
 
 
-transformations = read_file_data("data/latin_alphabet/czech-dev")
+def find_gap_positions_and_matching_characters(gap_string: str, character_string: str) -> List[
+    Operation]:
+    assert len(gap_string) == len(character_string)
+    positions = [m.start() for m in re.finditer('-', gap_string)]
+    grouped_positions = group_consecutive_indexes(positions)
+    operations = []
+    for group in grouped_positions:
+        if len(group) == 0:
+            continue
+        characters = ""
+        for index in group:
+            characters += character_string[index]
+        operations.append(Operation(group[0], characters))
+    return operations
 
-first = transformations[0]
-print(first)
-matrix = matlist.blosum62
-for a in pair.align.globaldx(first.lemma, first.inflection, matrix):
-    print(pair.format_alignment(*a))
+
+def group_consecutive_indexes(indexes: List[int]) -> List[List[int]]:
+    return [list(group) for group in mit.consecutive_groups(indexes)]
+
+
+def get_operations(transformations: List[Transformation]) -> List[Operations]:
+    return [find_operations(transformation) for transformation in transformations]
+
+
+def find_operations(transformation: Transformation) -> Operations:
+    # Large penalty to make sure no mismatching characters appear
+    penalty = -1000000
+    alignements = pair.align.globalms(transformation.lemma, transformation.inflection, 1, penalty,
+                                      -1, -0.1)
+    first_alig = alignements[0]
+    inserts = find_gap_positions_and_matching_characters(first_alig[0], first_alig[1])
+    deletes = find_gap_positions_and_matching_characters(first_alig[1], first_alig[0])
+    return Operations(transformation, inserts, deletes)
+
+
+trans_data = read_file_data("data/latin_alphabet/danish-dev")
+for i in range(len(trans_data)):
+    tran = trans_data[i]
+    print(i)
+    print(tran)
+    print(find_operations(tran))
+# operations = get_operations(trans_data)
