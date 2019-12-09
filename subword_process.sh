f1e51c167ef06e170ebd304ff046da17254725fc
Sabin Rimniceanu <sabin.rimniceanu@gmail.com>
2019-12-09 13:51:06 +0100

    Fixed subword

--- subword_process.sh
+++ subword_process.sh
@@ -1,2 +1,2 @@
-subword-nmt learn-bpe -s 2 < ./data/processed/third_step.txt > ./data/processed/fourth_step.txt
+subword-nmt learn-bpe -s 30 < ./data/processed/third_step.txt > ./data/processed/fourth_step.txt
 subword-nmt apply-bpe -c ./data/processed/fourth_step.txt < ./data/processed/fourth_step.txt > ./data/processed/fifth_step.txt
