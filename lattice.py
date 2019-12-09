829871725496fb93bbfc78d5e1870ac5effa498a
antaranin <rafal@getlevelup.com>
2019-12-09 15:59:19 +0100

    Recreated the step_3_files

--- lattice.py
+++ lattice.py
@@ -13,12 +13,19 @@ class Lattice:
     def find_operations_for_context(self, context: str) -> pd.DataFrame:
         if context not in self.context_matrix:
             return pd.DataFrame()
-        filtered_rows = self._filter_rows_below_threshold(context)
+        filtered_rows = self._filter_rows_below_threshold(context, self.context_matrix)
         return self._filter_columns_below_threshold(filtered_rows)
 
-    def _filter_rows_below_threshold(self, column: str) -> pd.DataFrame:
-        above_threshold = self.context_matrix[column] >= self.threshold
-        filtered = self.context_matrix[above_threshold]
+    def find_concept_for_object(self, object: str) -> pd.DataFrame:
+        transposed_matrix = self.context_matrix.transpose()
+        if object not in transposed_matrix:
+            return pd.DataFrame()
+        filtered_rows = self._filter_rows_below_threshold(object,transposed_matrix)
+        return self._filter_columns_below_threshold(filtered_rows).transpose()
+
+    def _filter_rows_below_threshold(self, column: str, matrix) -> pd.DataFrame:
+        above_threshold = matrix[column] >= self.threshold
+        filtered = matrix[above_threshold]
         return filtered
 
     def _filter_columns_below_threshold(self, filtered_rows: pd.DataFrame) -> pd.DataFrame:
--- runner.py
+++ runner.py
@@ -33,8 +33,10 @@ def generate_step_4():
         print(f"Finished work on {language}")
 
 
-ctx = load_context_matrix("data/processed/fourth_step/danish.csv")
-print(ctx)
-lattice = Lattice(ctx, 1)
-print(lattice.find_operations_for_context("en"))
+# ctx = load_context_matrix("data/processed/fourth_step/danish.csv")
+# print(ctx)
+# lattice = Lattice(ctx, 1)
+# print(lattice.find_operations_for_context("en"))
+# print(lattice.find_concept_for_object("INS(ser)"))
+generate_steps_1_2_3(generate_step_3=True)
 
